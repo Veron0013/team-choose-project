9e4c43527c466d6e7d6fffc3ec2af1ddee97e77d
Mr.Igor <iv_mirsoft@ukr.net>
2025-04-11 18:36:34 +0300

    js swiper experts

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const swiper_experts = new Swiper('.experts_swiper', {
+	loop: true,
+	slidesPerView: 3.8,
+	spaceBetween: 30,
+
+	navigation: {
+		nextEl: '.right',
+		prevEl: '.left',
+	},
+
+});
