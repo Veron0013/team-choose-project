e10499b9ac6c1ff220daa936e980169c9a608106
Mr.Igor <iv_mirsoft@ukr.net>
2025-05-15 21:43:57 +0300

    js switcher

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+//global
+let currentLanguage = 'ua';
+
+const switcher = document.querySelector(".hero #lang_input");
+console.log(switcher);
+
+//switcher
+function loadText() {
+	console.log("magic place here");
+}
+
+function changeLanguage() {
+
+	currentLanguage = switcher.checked ? 'en' : 'ua';
+	console.log(currentLanguage);
+}
+
+loadText();
+
+switcher.addEventListener("click", changeLanguage);
+
+
+//swiper
 const swiper_experts = new Swiper('.experts_swiper', {
 	loop: true,
 	slidesPerView: 3.8,
@@ -8,9 +31,9 @@ const swiper_experts = new Swiper('.experts_swiper', {
 		nextEl: '.right',
 		prevEl: '.left',
 	},
-	scrollbar: {
-		el: ".swiper-scrollbar",
-		hide: true,
-	},
+	//scrollbar: {
+	//	el: ".swiper-scrollbar",
+	//	hide: true,
+	//},
 
 });
