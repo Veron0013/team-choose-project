f2994c13fdb16add600092e7dc9b0794c2be34cf
Tetiana <Korostijtatiana@gmail.com>
2025-05-21 14:06:56 +0300

    add files

--- src/main.js
+++ src/main.js
@@ -1,39 +1,50 @@
 //global
 let currentLanguage = 'ua';
 
-const switcher = document.querySelector(".hero #lang_input");
+const switcher = document.querySelector('.hero #lang_input');
 console.log(switcher);
 
 //switcher
 function loadText() {
-	console.log("magic place here");
+  fetch('./public/text.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const dict = data.section['#features'][currentLanguage];
+
+      for (const selector in dict) {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(el => {
+          el.textContent = dict[selector];
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error loading or parsing JSON:', error);
+    });
 }
 
 function changeLanguage() {
-
-	currentLanguage = switcher.checked ? 'en' : 'ua';
-	console.log(currentLanguage);
+  currentLanguage = switcher.checked ? 'en' : 'ua';
+  console.log(currentLanguage);
+  loadText();
 }
 
-loadText();
-
-switcher.addEventListener("click", changeLanguage);
-
+switcher.addEventListener('click', changeLanguage);
 
 //swiper
 const swiper_experts = new Swiper('.experts_swiper', {
-	loop: true,
-	slidesPerView: 3.8,
-	spaceBetween: 34,
-	grabCursor: true,
+  loop: true,
+  slidesPerView: 3.8,
+  spaceBetween: 34,
+  grabCursor: true,
 
-	navigation: {
-		nextEl: '.right',
-		prevEl: '.left',
-	},
-	//scrollbar: {
-	//	el: ".swiper-scrollbar",
-	//	hide: true,
-	//},
-
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+  //scrollbar: {
+  //	el: ".swiper-scrollbar",
+  //	hide: true,
+  //},
 });
